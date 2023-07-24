43ada3a6bf97a7a3a02ec48d9eb65257a2f5290f
Luka Kresoja <57074484+LukeOnuke@users.noreply.github.com>
2023-07-24 14:03:21 +0200

    Stopped using wget by default, and now reading env vars

--- src/main/java/com/lukeonuke/Main.java
+++ src/main/java/com/lukeonuke/Main.java
@@ -6,9 +6,8 @@ import com.lukeonuke.model.CFFile;
 import com.lukeonuke.model.CFProject;
 import com.lukeonuke.model.Manifest;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,33 +21,40 @@ import static java.lang.System.*;
 public class Main {
     private static final DecimalFormat df = new DecimalFormat("0.00");
     public static String[] ARGUMENTS;
+    public static String API_KEY;
 
     public static void main(String[] args) {
         ARGUMENTS = args;
         if (Objects.equals(args[0], "help")) {
-            out.println("MPDL v1.0.0 - Commandline curseforge modpack downloader/assembler");
+            out.println("MPDL v0.0.1 - Commandline curseforge modpack downloader/assembler");
             out.println("by lukeonuke (https://github.com/LukeOnuke)");
             out.println();
-            out.println("Usage : mpdl [modpack-dir] [curseforge-api-key]");
-            err.println("ex: java -jar mpdl.jar \"c://Users/me/Downloads/modpack\" '$2a$10$xG3FcKtxFaELSwuZtBUideqKkLnTn9ODNp0GvIBBST/o/Umm4cyz2'");
-            System.exit(0);
+            out.println("Usage : mpdl [curseforge-api-key] [modpack-dir]");
+            out.println("ex: java -jar mpdl.jar '$2a$10$xG3FcKtxFaELSwuZtBUideqKkLnTn9ODNp0GvIBBST/o/Umm4cyz2' \"c://Users/me/Downloads/modpack\"");
+            out.println();
+            out.println("You can also set an enviroment variable named API_KEY to the api key and skip declaring it in the arguments.");
+            exit(0);
         }
 
-        if (args.length < 2) {
-            err.println("ERROR: No modpack directory provided or api key.");
-            err.println("--------");
-            err.println("Provide modpack directory as the first argument of this application.");
-            err.println("Provide API key as the second argument of this application.");
-            err.println("--------");
-            err.println("ex: java -jar mpdl.jar \"c://Users/me/Downloads/modpack\" '$2a$10$xG3FcKtxFaELSwuZtBUideqKkLnTn9ODNp0GvIBBST/o/Umm4cyz2'");
-            exit(1);
-        }
+//        if (args.length < 2) {
+//            err.println("ERROR: No modpack directory provided or api key.");
+//            err.println("--------");
+//            err.println("Provide modpack directory as the first argument of this application.");
+//            err.println("Provide API key as the second argument of this application.");
+//            err.println("--------");
+//            err.println("ex: java -jar mpdl.jar \"c://Users/me/Downloads/modpack\" '$2a$10$xG3FcKtxFaELSwuZtBUideqKkLnTn9ODNp0GvIBBST/o/Umm4cyz2'");
+//            exit(1);
+//        }
 
+        API_KEY = args[0];
+        // Read api key from env
+        if(!Objects.isNull(getenv("API_KEY"))) API_KEY = getenv("API_KEY");
 
-        final Path modpackFolder = Path.of(args[0]);
+        Path modpackFolder = Path.of(args[0]);
+        if(!modpackFolder.toFile().exists()) modpackFolder = Path.of(args[1]);
 
         out.println("Modpack folder is " + modpackFolder);
-        out.println("Token is " + args[1]);
+        out.println("Token is " + API_KEY);
         final Path manifestPath = modpackFolder.resolve("manifest.json");
 
         out.println("Loading manifest.json from " + manifestPath);
@@ -154,21 +160,44 @@ public class Main {
     }
 
     public static String getUrl(String url) throws IOException {
-        ProcessBuilder builder = new ProcessBuilder();
-        builder.command(
-                "script", "-c",
-                "wget -q -O - " +
-                        "--header='Accept: application/json' " +
-                        "--header='x-api-key: " + ARGUMENTS[1] + "' " +
-                        "--header='User-Agent: Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' " +
-                        url,
-                "-f",
-                "-q"
-        );
-        Process wget = builder.start();
-        String stringReturn = new String(wget.getInputStream().readAllBytes());
+//        ProcessBuilder builder = new ProcessBuilder();
+//        builder.command(
+//                "script", "-c",
+//                "wget -q -O - " +
+//                        "--header='Accept: application/json' " +
+//                        "--header='x-api-key: " + ARGUMENTS[1] + "' " +
+//                        "--header='User-Agent: Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' " +
+//                        url,
+//                "-f",
+//                "-q"
+//        );
+//        Process wget = builder.start();
+//        String stringReturn = new String(wget.getInputStream().readAllBytes());
+//
+//        wget.destroy();
+//        return stringReturn;
 
-        wget.destroy();
-        return stringReturn;
+        URL urlObject = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) urlObject.openConnection();
+        con.setRequestMethod("GET");
+
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("x-api-key", API_KEY);
+        con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
+
+        con.connect();
+        if (con.getResponseCode() != 200) throw new IOException("RESPONSE CODE NOT HTTP/OK, actual value " + con.getResponseCode() +  " " + con.getResponseMessage());
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+
+        return content.toString();
     }
 }
